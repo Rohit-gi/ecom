b758012caa8b4c6800b12f906a8a988ccdd24c62
Rohit-gi <i.rohit003@gmail.com>
2023-08-17 13:37:10 +0530

    Updated Products component

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -19,23 +19,21 @@ const Info = styled.div`
  width: 100%;
  height: 100%;
  display: flex;
- align-items: center;
+ align-items: center;   
  flex-direction: column;
- justify-content: center;
-
+ justify-content: center; 
 `
 const Title = styled.h1`
   color: white;
-  margin-bottom: 20px;
-`
+  margin-bottom: 20px;          
+` 
 const Button = styled.button`
   border: none;
-  padding: 10px;
-  background-color: white;
+  padding: 10px;              
+  background-color: white;                
   color: gray;
   cursor: pointer;
   font-weight: 600;
-
 `
 
 const CategoryItem = ({item}) => {
